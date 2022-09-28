6acebe11c627cb6242ae093249c5fdd1fd9eb5a9
dajim10 <dajim10@hotmail.com>
2022-09-28 21:27:41 +0700

    commit at home again

--- server/server.js
+++ server/server.js
@@ -1,27 +1,33 @@
 require('dotenv').config();
 const express = require("express");
-const multiparty = require("connect-multiparty")
-const multipartyMiddleware = multiparty({ uploadDir : './public/images'})
+// const multiparty = require("connect-multiparty")
+// const multipartyMiddleware = multiparty({ uploadDir : './public/images'})
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const app = express();
 const mysql = require('mysql2')
+const multer = require('multer')
+const path = require('path')
 const PORT = process.env.PORT;
 const SERVER = process.env.SERVER;
 const IMAGE_UPLOAD_DIR = "./public/images/"
 const IMAGE_BASE_URL = process.env.IMAGEURL;
 
-app.use(bodyParser.urlencoded({ extended: false}))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false}))
 
-app.use(express.static("public"));
+
+
+
+app.use(express.static("./public"));
 app.use(cors());
 
 const connection = mysql.createConnection({
     host: process.env.HOST,
     user: process.env.USER,
     password: process.env.PASSWORD,
-    database: process.env.DATABASE
+    database: process.env.DATABASE,
+    port:3306,
 })
 
 connection.connect((err, result) => {
@@ -30,10 +36,133 @@ connection.connect((err, result) => {
 });
 
 
+app.post("/upload_files",(req,res)=>{
+    if (req.files.length > 0) {
+        res.json(req.files[0]);
+    }
+})
+
+app.get("/sdgs",(req,res)=> {
+    connection.query(`SELECT * FROM sdg_goal`,function(err,result){
+        if (err) throw err;
+        res.send(result);
+    })
+})
+// ! Use of Multer
+var storage = multer.diskStorage({
+    destination: (req,file,callBack) =>{
+        callBack(null,'./public')
+    },
+    filename: (req,file,callBack) => {
+        callBack(null,Date.now() + path.extname(file.originalname))
+    }
+})
+
+var upload = multer({
+    storage : storage
+})
+
+
 app.get('/', (req, res) => {
     console.log("Home...")
 })
+// upload file to sever
 
+app.post("/ckupload",upload.single("image"),(req,res)=>{
+    if (!req.file) {
+        res.status(400).send();
+    }
+})
+
+app.get("/content/:id",(req,res)=>{
+    const id = req.params.id;
+    connection.query("SELECT * FROM content WHERE id = ?",[id],(err,result)=>{
+        if (err) throw err;
+        res.send(result)
+    })
+})
+
+app.post("/update/:id",upload.array("image"),(req,res)=>{
+    const id = req.params.id;
+    if (!id) {
+        res.status(400).send({message:"error 404 "})
+    }else {
+
+        console.log(req.params.id);
+    }
+})
+
+app.post("/upload",upload.single("image"),(req,res)=>{
+    if (!req.file) {
+        res.status(400).send();
+    }else {
+        console.log(req.file)
+        console.log(req.body)
+        var imgsrc = `${process.env.SERVER}/`+req.file.filename
+        var content_name = req.body.content_name;
+        var content_detail = req.body.content_detail;
+        const sdgFrontEnd = req.body.sdgID
+        const sdgID=sdgFrontEnd.split(',').map(x=>+x);
+        
+        // b="1,2,3,4".split(',').map(x=>+x)
+        console.log(sdgID)
+        console.log(content_name,content_detail)
+        var insertData = "INSERT INTO content (content_name,content_detail,image) VALUES(?,?,?)"
+        connection.query(insertData,[content_name,content_detail,imgsrc],(err,result)=>{
+            if (err){
+                throw err  
+            } else {
+                console.log("file Upload "+result);
+                console.log(result.insertId);
+                const ContentID = result.insertId;
+                // sdgID.forEach(function(number){
+                sdgID.sort(function (a,b) {return a - b}).forEach(function (number){
+                    connection.query(
+                        "INSERT INTO content_sdg(content_id,sdg_number) VALUES (?,?) ",
+                        [ContentID,number],
+                        (err,result) => {
+                            if(err) throw err;
+                        }
+                    )
+                })
+
+            }
+
+        })
+        res.status(200).send();
+    }
+})
+
+// app.post("/upload",upload.single("image"),(req,res)=>{
+//     if (!req.file) {
+//         console.log("No file upload");
+//     }else {
+//         console.log(req.file.filename)
+//         // const content_name = req.content_name;
+//         // const content_detail = req.content_detail;
+//         var imgsrc = "http://localhost:4000/images/"+req.file.filename
+//         var insertData = "INSERT INTO content (image) VALUES(?)"
+//         connection.query(insertData,[imgsrc],(err,result)=>{
+//             if(err) throw err
+//             console.log("file Uploaded")
+//         })
+//     }
+// })
+
+
+// app.post('/insertdata',(req,res) =>{
+//     console.log(req);
+//     const content_name = req.body.content_name;
+//     const image = req.files;
+//     const content_detail = req.body.content_detail;
+    
+
+//     connection.query("INSERT INTO content (content_name,image,content_detail) VALUES (?,?,?)",[content_name,image,content_detail],(err,result)=>{
+//         if (err) throw err;
+//         res.send(result)
+
+//     })
+// })
 app.get('/content', (req, res) => {
     connection.query("SELECT * FROM content ORDER BY ID DESC ", (err, result, fields) => {
         if (err) throw err;
@@ -41,42 +170,30 @@ app.get('/content', (req, res) => {
     })
 })
 
-app.post('/insertdata',(req,res) =>{
-    console.log(req);
-    const content_name = req.body.content_name;
-    const image = req.body.image;
-    console.log(content_name,image);
-
-    connection.query("INSERT INTO content (content_name,image) VALUES (?,?)",formData,(err,result)=>{
-        if (err) throw err;
-        res.send(result)
-
-    })
-})
 
 
 
 
 
 // upload image with multer
-const multer = require('multer');
-var storege = multer.diskStorage({
-    destination: function (req,file,cb) {
-        cb(null,"./public/images");
-    },
-    filename : (req,file,cb) => {
-        cb(null,Date.now()+"--"+file.originalname);
-    },
-})
-const upload = multer({ storage: storege});
+// const multer = require('multer');
+// var storege = multer.diskStorage({
+//     destination: function (req,file,cb) {
+//         cb(null,"./public/images");
+//     },
+//     filename : (req,file,cb) => {
+//         cb(null,Date.now()+"--"+file.originalname);
+//     },
+// })
+// const upload = multer({ storage: storege});
 
-app.post('/upload',upload.single("file"),(req,res)=>{
-    let form = new multiparty.Form({uploadDir : IMAGE_UPLOAD_DIR});
-    form.parse(req,function(err,fields,files){
-        if (err ) return res.send({ err : err.messageq});
-        console.log(`fields= ${JSON.stringify(fields,null,2)}`)
-    })
-})
+// app.post('/upload',upload.single("file"),(req,res)=>{
+//     let form = new multiparty.Form({uploadDir : IMAGE_UPLOAD_DIR});
+//     form.parse(req,function(err,fields,files){
+//         if (err ) return res.send({ err : err.messageq});
+//         console.log(`fields= ${JSON.stringify(fields,null,2)}`)
+//     })
+// })
 
 app.post("/addcontent",(req,res)=>{
     // let form = new multiparty.Form({ uploadDir : IMAGE_UPLOAD_DIR});
